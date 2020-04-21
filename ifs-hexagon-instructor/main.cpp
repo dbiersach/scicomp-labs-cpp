af8a6da51398d7d48ba64aeddc2d798323bb4302
Dave Biersach <dbiersach@gmail.com>
2020-04-21 17:30:16 +0000

    Initial commit

---
+++ ifs-hexagon-instructor/main.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "SimpleScreen.h"
+#include "IteratedFunctionSystem.h"
+
+using namespace std;
+
+IteratedFunctionSystem* ifs = nullptr;
+const int iterations = 100000;
+
+void draw(SimpleScreen& ss) {
+	ss.LockDisplay();
+	double x = 0, y = 0;
+	ALLEGRO_COLOR clr;
+	for (int i{}; i < iterations; i++) {
+		ifs->TransformPoint(x, y, clr);
+		while (x < ss.worldXmin || x > ss.worldXmax
+			|| y < ss.worldYmin || y > ss.worldYmax)
+			ifs->TransformPoint(x, y, clr);
+		ss.DrawPoint(x, y, clr);
+	}
+	ss.UnlockDisplay();
+}
+
+
+int main()
+{
+	SimpleScreen ss(draw);
+	ss.SetZoomFrame("white", 3);
+
+	ss.SetWorldRect(0, 0, 30, 30);
+
+	ifs = new IteratedFunctionSystem();
+
+	ifs->SetBaseFrame(0, 0, 30, 30);
+
+	double p = 1. / 6;
+
+	double h = 10.0 * sqrt(3) / 2.0;  // 10 * sin(60 degrees)
+
+	ifs->AddMapping(25, 15, 15, 15, 20, 15 + h, "blue", p);			// COD
+	ifs->AddMapping(20, 15 + h, 15, 15, 10, 15 + h, "blue", p);		// DOE
+	ifs->AddMapping(10, 15 + h, 15, 15, 5, 15, "blue", p);			// EOF
+	ifs->AddMapping(5, 15, 15, 15, 10, 15 - h, "blue", p);			// FOA
+	ifs->AddMapping(10, 15 - h, 15, 15, 20, 15 - h, "blue", p);		// AOB
+	ifs->AddMapping(20, 15 - h, 15, 15, 25, 15, "blue", p); 		// BOC
+
+	ifs->GenerateTransforms();
+
+	ss.HandleEvents();
+
+	delete ifs;
+
+	return 0;
+}
+
