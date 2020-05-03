b88d8edd570c58ec1065c8c771a6a5fcf8ce7b4e
Dave Biersach <dbiersach@gmail.com>
2020-05-03 03:49:16 +0000

    Update Session 05

--- draw-rectangle-instructor/main.cpp
+++ draw-rectangle-instructor/main.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 int main()
 {
-	SimpleScreen ss;
-	ss.SetWorldRect(-30, -30, 30, 30);
-	ss.DrawAxes();
+    SimpleScreen ss;
+    ss.SetWorldRect(-30, -30, 30, 30);
+    ss.DrawAxes();
 
-	PointSet ps;
-	ps.add(0, 0);
-	ps.add(21, 0);
-	ps.add(21, 10);
-	ps.add(0, 10);
+    PointSet ps;
+    ps.add(0, 0);
+    ps.add(21, 0);
+    ps.add(21, 10);
+    ps.add(0, 10);
 
-	ss.DrawLines(&ps, "violet", 1, true, true);
+    ss.DrawLines(&ps, "violet", 1, true, true);
 
-	ss.HandleEvents();
+    ss.HandleEvents();
 
-	return 0;
+    return 0;
 }
--- draw-rectangle/main.cpp
+++ draw-rectangle/main.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 int main()
 {
-	SimpleScreen ss;
-	ss.SetWorldRect(-30, -30, 30, 30);
-	ss.DrawAxes();
+    SimpleScreen ss;
+    ss.SetWorldRect(-30, -30, 30, 30);
+    ss.DrawAxes();
 
-	PointSet ps;
-	ps.add(0, 0);
-	ps.add(5, 0);
-	ps.add(5, 5);
-	ps.add(0, 5);
+    PointSet ps;
+    ps.add(0, 0);
+    ps.add(5, 0);
+    ps.add(5, 5);
+    ps.add(0, 5);
 
-	ss.DrawLines(&ps, "violet", 1, true, true);
+    ss.DrawLines(&ps, "violet", 1, true, true);
 
-	ss.HandleEvents();
+    ss.HandleEvents();
 
-	return 0;
+    return 0;
 }
