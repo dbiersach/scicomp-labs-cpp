f66c378926a8a4466dd2111783a8eba489eff013
Dave Biersach <dbiersach@gmail.com>
2020-05-09 05:40:28 +0000

    Fix Session09

--- jenga-14/blocks.cpp
+++ jenga-14/blocks.cpp
@@ -5,101 +5,97 @@ using namespace std;
 
 Point2D::Point2D()
 {
-	x = 0;
-	y = 0;
+    x = 0;
+    y = 0;
 }
 
 Point2D::Point2D(double x, double y)
 {
-	this->x = x;
-	this->y = y;
+    this->x = x;
+    this->y = y;
 }
 
 Point2D::~Point2D()
 {
-	// Empty destructor
+    // Empty destructor
 }
 
 
 Block::Block()
 {
-	center = nullptr;
+    center = nullptr;
 }
 
 Block::Block(const Block &rhs)
 {
-	center = new Point2D(rhs.center->x, rhs.center->y);
+    center = new Point2D(rhs.center->x, rhs.center->y);
 }
 
 Block::Block(double x, double y)
 {
-	center = new Point2D(x, y);
+    center = new Point2D(x, y);
 }
 
 Block::~Block()
 {
-	delete center;
+    delete center;
 }
 
 void Block::Move(double deltaX, double deltaY)
 {
-	center->x += deltaX;
-	center->y += deltaY;
+    center->x += deltaX;
+    center->y += deltaY;
 }
 
 
 BlockList::BlockList()
 {
-	blocks = new vector<Block>();
-	center = new Point2D();
+    blocks = new vector<Block>();
+    center = new Point2D();
 }
 
 BlockList::~BlockList()
 {
-	blocks->clear();
-	delete blocks;
-	delete center;
+    blocks->clear();
+    delete blocks;
+    delete center;
 }
 
 int BlockList::Count()
 {
-	return blocks->size();
+    return blocks->size();
 }
 
 void BlockList::AddBlock(double x, double y)
 {
-	blocks->push_back(Block(x, y));
+    blocks->push_back(Block(x, y));
 }
 
 void BlockList::MoveBlocks(double deltaX, double deltaY)
 {
-	for (auto &block : *blocks)
-		block.Move(deltaX, deltaY);
+    for (auto &block : *blocks)
+        block.Move(deltaX, deltaY);
 }
 
 void BlockList::CalcCenter()
 {
-	center->x = 0;
-	center->y = 0;
-	for (const auto &block : *blocks) {
-		center->x += block.center->x;
-		center->y += block.center->y;
-	}
-	center->x /= blocks->size();
-	center->y /= blocks->size();
+    center->x = 0;
+    center->y = 0;
+    for (const auto &block : *blocks)
+    {
+        center->x += block.center->x;
+        center->y += block.center->y;
+    }
+    center->x /= blocks->size();
+    center->y /= blocks->size();
 
-	cout << "Blocks: ";
-	cout << setw(3) << blocks->size();
-	cout << "\tCenter X:";
-	cout << setw(7) << setprecision(2) << fixed;
-	cout << center->x;
-	cout << "\tCenter Y:";
-	cout << setw(7) << setprecision(2) << fixed;
-	cout << center->y;
-	cout << endl;
+    cout << "Blocks: ";
+    cout << setw(3) << blocks->size();
+    cout << "\tCenter X:";
+    cout << setw(7) << setprecision(2) << fixed;
+    cout << center->x;
+    cout << "\tCenter Y:";
+    cout << setw(7) << setprecision(2) << fixed;
+    cout << center->y;
+    cout << endl;
 }
-
-
-
-
-
