90e6eac889c870c59fee23e727287a45550b01ca
David Biersach <dbiersach@gmail.com>
2020-05-14 00:53:02 -0400

    Update stdafx.h

--- caesar-decrypt-instructor/stdafx.h
+++ caesar-decrypt-instructor/stdafx.h
@@ -1,32 +1,31 @@
-// stdafx.h
+﻿#pragma once
 
-#pragma once
-
-#include <stdio.h>
-#include <time.h>
 #define _USE_MATH_DEFINES
-#include <math.h>
+
 #include <algorithm>
 #include <array>
-#include <assert.h>
+#include <atomic>
+#include <cassert>
 #include <chrono>
 #include <cmath>
+#include <complex>
 #include <cstdio>
+#include <ctime>
 #include <fstream>
 #include <functional>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <locale>
-#include <memory>
 #include <map>
+#include <memory>
+#include <mutex>
 #include <random>
 #include <sstream>
 #include <string>
 #include <thread>
-#include <time.h>
 #include <tuple>
 #include <unordered_map>
 #include <utility>
 #include <vector>
-
