488f07b08dc13fa83957cabff599d4f338a5a5dd
David Biersach <dbiersach@gmail.com>
2020-05-24 16:18:12 -0400

    Update lab

--- decay_carbon14_instructor/decay_carbon14.cpp
+++ decay_carbon14_instructor/decay_carbon14.cpp
@@ -4,44 +4,42 @@ using namespace std;
 
 void decay_carbon14()
 {
-    // Set number of time steps in simulation
-    const int timeSteps{ 100 };
-
-    double timeAt[timeSteps];
-    double nuclei[timeSteps];
-
-    // Set percent of nuclei initially present
-    nuclei[0] = 100;
-
     // Half-life of Carbon-14 (years)
-    const double halfLife{ 5730 };
+    const double halfLife{5730};
 
     // Duration of simulation (years)
-    const double endTime{ 40000 };
+    const double endTime{40000};
+
+    // Set number of time steps in simulation
+    const int timeSteps{100};
 
     // Calculate time step (delta t)
-    const double deltaTime{ endTime / timeSteps };
+    const double deltaTime{endTime / timeSteps};
 
     // Calculate decay factor
     const double decayFactor = deltaTime / halfLife;
 
-    // Set initial time
-    timeAt[0] = 0.0;
+    // Initialize domain and range vectors
+    vector<double> time(timeSteps, 0);
+    vector<double> nuclei(timeSteps, 0);
+
+    // Set percent of nuclei initially present
+    nuclei.at(0) = 100;
 
     // Perform Euler method to estimate differential equation
     for (int step{}; step < timeSteps - 1; ++step)
     {
-        nuclei[step + 1] = nuclei[step] - nuclei[step] * decayFactor;
-        timeAt[step + 1] = timeAt[step] + deltaTime;
+        nuclei.at(step + 1) = nuclei.at(step) - nuclei.at(step) * decayFactor;
+        time.at(step + 1) = time.at(step) + deltaTime;
     }
 
     // Graph the decay curve using CERN's ROOT libraries
-    TCanvas* c1 = new TCanvas("Carbon-14 Tau Graph");
+    TCanvas *c1 = new TCanvas("Fluorine-18 Tau Graph");
     c1->SetTitle("Lab 1 - Nuclear Decay");
 
-    TGraph* g1 = new TGraph(timeSteps, timeAt, nuclei);
+    TGraph *g1 = new TGraph(timeSteps, time.data(), nuclei.data());
 
-    g1->SetTitle("Radioactive Decay of Carbon-14;time (yrs);% of Original Amount");
+    g1->SetTitle("Radioactive Decay of Carbon-14;time (h);% of Original Amount");
     g1->SetMarkerStyle(kFullDotMedium);
     g1->SetLineColor(2);
     g1->Draw();
