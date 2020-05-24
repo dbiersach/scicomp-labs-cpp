cefa3be3a4b26d3fd5efb7adddf67f815dc3a0ca
David Biersach <dbiersach@gmail.com>
2020-05-24 13:54:52 -0400

    Format

--- decay_fluorine18/decay-flourine18.cpp
+++ decay_fluorine18/decay-flourine18.cpp
@@ -5,16 +5,16 @@ using namespace std;
 void decay_flourine18()
 {
     // Half-life of Fluorine-18 (secs to hours)
-    const double halfLife{ 6586.0 / 60 / 60 };
+    const double halfLife{6586.0 / 60 / 60};
 
     // Set number of time steps in simulation
-    const int timeSteps{ 100 };
+    const int timeSteps{100};
 
     // Duration of simulation (hours)
-    const double endTime{ 12 };
+    const double endTime{12};
 
     // Calculate time step (delta t)
-    const double deltaTime{ endTime / timeSteps };
+    const double deltaTime{endTime / timeSteps};
 
     // Calculate decay factor
     const double decayFactor = deltaTime / halfLife;
@@ -34,10 +34,10 @@ void decay_flourine18()
     }
 
     // Graph the decay curve using CERN's ROOT libraries
-    TCanvas* c1 = new TCanvas("Fluorine-18 Tau Graph");
+    TCanvas *c1 = new TCanvas("Fluorine-18 Tau Graph");
     c1->SetTitle("Lab 1 - Nuclear Decay");
 
-    TGraph* g1 = new TGraph(timeSteps, time.data(), nuclei.data());
+    TGraph *g1 = new TGraph(timeSteps, time.data(), nuclei.data());
 
     g1->SetTitle("Radioactive Decay of Fluorine-18;time (h);% of Original Amount");
     g1->SetMarkerStyle(kFullDotMedium);
