416ade00c8a631b0e75dc90b69872e882c75c85d
Dave Biersach <dbiersach@gmail.com>
2020-05-24 21:32:43 +0000

    Add rk4 decay

--- rk4_decay_carbon14/rk4_decay_carbon14.cpp
+++ rk4_decay_carbon14/rk4_decay_carbon14.cpp
@@ -2,32 +2,35 @@
 
 using namespace std;
 
+// Half-live of Carbon-14 (years)
+const double halfLife{5730};
+
+// Duration of simulation (years)
+const double endTime{40000};
+
+// Set number of time steps in simulation
+const int timeSteps{100};
+
 // The differential equation
 double f(double y, double x)
 {
-	return (-x) / 5730;  // Half-life of Carbon-14 (years)
+    return -(y / halfLife);
 }
 
-// Fourth-Order Runge-Kutta Method for a single 1st order differential equation
-void runge_kutta(double &y, double &x, const double dx,
-	double(*f)(double, double))
+// Fourth-Order Runge-Kutta Method (single ODE)
+void runge_kutta4(double &y, double &x, const double dx,
+                  double (*f)(double, double))
 {
-	double k1 = f(y, x);
-	double k2 = f(y + k1 * dx / 2, x + dx / 2);
-	double k3 = f(y + k2 * dx / 2, x + dx / 2);
-	double k4 = f(y + k3 * dx, x + dx);
-	y += dx / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
-	x += dx;
+    double k1 = f(y, x);
+    double k2 = f(y + k1 * dx / 2, x + dx / 2);
+    double k3 = f(y + k2 * dx / 2, x + dx / 2);
+    double k4 = f(y + k3 * dx, x + dx);
+    y += dx / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+    x += dx;
 }
 
 void rk4_decay_carbon14()
 {
-    // Duration of simulation (years)
-    const double endTime{40000};
-
-    // Set number of time steps in simulation
-    const int timeSteps{100};
-
     // Calculate time step
     const double deltaTime{endTime / timeSteps};
 
@@ -38,15 +41,15 @@ void rk4_decay_carbon14()
     // Set percent of nuclei initially present
     nuclei.at(0) = 100;
 
-    double x = time.at(0);
-    double y = nuclei.at(0);
+    double x = time.at(0);   // Independent
+    double y = nuclei.at(0); // Dependent
 
-    // Perform Euler method to estimate differential equation
+    // Use RK4 to estimate the differential equation
     for (int step{1}; step < timeSteps; ++step)
     {
-        runge_kutta(y, x, deltaTime, f);        
+        runge_kutta4(y, x, deltaTime, f);
         time.at(step) = x;
-        nuclei.at(step) = y;        
+        nuclei.at(step) = y;
     }
 
     // Graph the decay curve using CERN's ROOT libraries
