bf6611ca4bc1d6a577185f67270c507f2520150a
David Biersach <dbiersach@gmail.com>
2020-05-24 18:03:09 -0400

    Update lab

--- pendulum/pendulum.cpp
+++ pendulum/pendulum.cpp
@@ -4,50 +4,49 @@ using namespace std;
 
 void pendulum()
 {
-    // Set number of time steps in simulation
-    const int timeSteps{ 250 };
+	const double length = 1.0; // (m)
+	const double g = 9.8;	   // (m/s^2)
+	const double phaseConstant = g / length;
 
-    double timeAt[timeSteps];
-    double omega[timeSteps];
-    double theta[timeSteps];
+	// Set number of time steps in simulation
+	const int timeSteps{250};
 
-    const double length = 1.0;	// (m)
-    const double g = 9.8;		// (m/s^2)
+	vector<double> time(timeSteps,0);
+	vector<double> omega(timeSteps,0);
+	vector<double> theta(timeSteps,0);
+	
+	// Set initial pendulum angular velocity
+	omega.at(0) = 0.0;
 
-    const double phaseConstant = g / length;
+	// Set initial pendulum displacement
+	theta.at(0) = M_PI / 18.0;
 
-    // Set initial pendulum angular velocity
-    omega[0] = 0.0;
+	// Duration of simulation (secs)
+	const double endTime{10};
 
-    // Set initial pendulum displacement
-    theta[0] = M_PI / 18.0;
+	// Calculate time step (delta t)
+	const double deltaTime{endTime / timeSteps};
 
-    // Duration of simulation (secs)
-    const double endTime{ 10 };
+	// Set initial time
+	timeAt.at(0) = 0.0;
 
-    // Calculate time step (delta t)
-    const double deltaTime{ endTime / timeSteps };
+	// Perform Euler method to estimate differential equation
+	for (int step{}; step < timeSteps - 1; ++step)
+	{
+		omega.at(step + 1) = omega.at(step) - phaseConstant * theta.at(step) * deltaTime;
+		theta.at(step + 1) = theta.at(step) + omega.step(step + 1) * deltaTime;
+		time.at(step + 1) = time.at(step) + deltaTime;
+	}
 
-    // Set initial time
-    timeAt[0] = 0.0;
+	// Graph the decay curve using CERN's ROOT libraries
+	TCanvas *c1 = new TCanvas("Harmonic Motion - Euler Method");
+	c1->SetTitle("Simple Pendulum - Euler Method");
 
-    // Perform Euler method to estimate differential equation
-    for (int step{}; step < timeSteps - 1; ++step)
-    {
-        omega[step + 1] = omega[step] - phaseConstant * theta[step] * deltaTime;
-        theta[step + 1] = theta[step] + omega[step] * deltaTime;
-        timeAt[step + 1] = timeAt[step] + deltaTime;
-    }
+	TGraph *g1 = new TGraph(timeSteps, time.data(), theta.data());
 
-    // Graph the decay curve using CERN's ROOT libraries
-    TCanvas* c1 = new TCanvas("Harmonic Motion - Euler Method");
-    c1->SetTitle("Simple Pendulum - Euler Method");
+	g1->SetTitle("Simple Pendulum - Euler Method;time (s);theta (radians)");
+	g1->SetLineColor(kBlue);
+	g1->SetLineWidth(3);
 
-    TGraph* g1 = new TGraph(timeSteps, timeAt, theta);
-
-    g1->SetTitle("Simple Pendulum - Euler Method;time (s);theta (radians)");
-    g1->SetLineColor(kBlue);
-    g1->SetLineWidth(3);
-
-    g1->Draw();
+	g1->Draw();
 }
--- pendulum_instructor/pendulum.cpp
+++ pendulum_instructor/pendulum.cpp
@@ -4,47 +4,47 @@ using namespace std;
 
 void pendulum()
 {
-	// Set number of time steps in simulation
-	const int timeSteps{ 250 };
-
-	double timeAt[timeSteps];
-	double omega[timeSteps];
-	double theta[timeSteps];
-
-	const double length = 1.0;	// (m)
-	const double g = 9.8;		// (m/s^2)
-
+	const double length = 1.0; // (m)
+	const double g = 9.8;	   // (m/s^2)
 	const double phaseConstant = g / length;
 
+	// Set number of time steps in simulation
+	const int timeSteps{250};
+
+	vector<double> time(timeSteps,0);
+	vector<double> omega(timeSteps,0);
+	vector<double> theta(timeSteps,0);
+	
 	// Set initial pendulum angular velocity
-	omega[0] = 0.0;
+	omega.at(0) = 0.0;
 
 	// Set initial pendulum displacement
-	theta[0] = M_PI / 18.0;
+	theta.at(0) = M_PI / 18.0;
 
 	// Duration of simulation (secs)
-	const double endTime{ 10 };
+	const double endTime{10};
 
 	// Calculate time step (delta t)
-	const double deltaTime{ endTime / timeSteps };
+	const double deltaTime{endTime / timeSteps};
 
 	// Set initial time
-	timeAt[0] = 0.0;
+	timeAt.at(0) = 0.0;
 
 	// Perform Euler method to estimate differential equation
-	for (int step{}; step < timeSteps - 1; ++step) {
-		omega[step + 1] = omega[step] - phaseConstant * theta[step] * deltaTime;
-		theta[step + 1] = theta[step] + omega[step + 1] * deltaTime;
-		timeAt[step + 1] = timeAt[step] + deltaTime;
+	for (int step{}; step < timeSteps - 1; ++step)
+	{
+		omega.at(step + 1) = omega.at(step) - phaseConstant * theta.at(step) * deltaTime;
+		theta.at(step + 1) = theta.at(step) + omega.step(step + 1) * deltaTime;
+		time.at(step + 1) = time.at(step) + deltaTime;
 	}
 
 	// Graph the decay curve using CERN's ROOT libraries
-	TCanvas* c1 = new TCanvas("Harmonic Motion - Euler Method");
-	c1->SetTitle("Simple Pendulum - Euler Method");
+	TCanvas *c1 = new TCanvas("Harmonic Motion - Euler-Cromer Method");
+	c1->SetTitle("Simple Pendulum - Euler-Cromer Method");
 
-	TGraph* g1 = new TGraph(timeSteps, timeAt, theta);
+	TGraph *g1 = new TGraph(timeSteps, time.data(), theta.data());
 
-	g1->SetTitle("Simple Pendulum - Euler Method;time (s);theta (radians)");
+	g1->SetTitle("Simple Pendulum - Euler-Cromer Method;time (s);theta (radians)");
 	g1->SetLineColor(kBlue);
 	g1->SetLineWidth(3);
 
