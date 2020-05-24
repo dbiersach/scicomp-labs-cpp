896ca1b1196d3d187bbcfc354027a196699acac3
David Biersach <dbiersach@gmail.com>
2020-05-24 17:35:57 -0400

    Update Canvas Title

--- decay_fluorine18/decay_fluorine18.cpp
+++ decay_fluorine18/decay_fluorine18.cpp
@@ -35,7 +35,7 @@ void decay_fluorine18()
 
     // Graph the decay curve using CERN's ROOT libraries
     TCanvas *c1 = new TCanvas("Fluorine-18 Tau Graph");
-    c1->SetTitle("Lab 1 - Nuclear Decay");
+    c1->SetTitle("Lab 2 - Nuclear Decay");
 
     TGraph *g1 = new TGraph(timeSteps, time.data(), nuclei.data());
 
