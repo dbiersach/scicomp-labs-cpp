ac38b132d5bac5822c1e626cbb42181c4f4348df
Dave Biersach <dbiersach@gmail.com>
2020-05-30 19:08:01 +0000

    Update labs

--- spectrum-rydberg-instructor/main.cpp
+++ spectrum-rydberg-instructor/main.cpp
@@ -9,13 +9,15 @@ int main()
 	cout << "Rydberg Formula Hydrogen Spectral Lines" << endl;
 	cout << fixed << setprecision(0);
 
+	// k is the final orbit #
 	for (int k{1}; k < 5; ++k)
 	{
+        // j is the initial orbit #
 		for (int j{k + 1}; j < k + 6; ++j)
 		{
 			double lambda = 1 / (R * (1 / pow(k, 2) - 1 / pow(j, 2)));
 			cout << setw(3) << j;
-			cout << setw(10) << lambda * 1e9 << "nm"
+			cout << setw(10) << lambda * 1e9 << "nm";
 			cout << endl;
 		}
 		// Skip a line between families
--- spectrum-rydberg/main.cpp
+++ spectrum-rydberg/main.cpp
@@ -9,13 +9,15 @@ int main()
 	cout << "Rydberg Formula Hydrogen Spectral Lines" << endl;
 	cout << fixed << setprecision(0);
 
+    // k is the final orbit #
 	for (int k{1}; k < 5; ++k)
 	{
+	    // j is the initial orbit #
 		for (int j{k + 1}; j < k + 6; ++j)
 		{
 			double lambda = 0;
 			cout << setw(3) << j;
-			cout << setw(10) << lambda * 1e9 << "nm"
+			cout << setw(10) << lambda * 1e9 << "nm";
 			cout << endl;
 		}
 		// Skip a line between families
