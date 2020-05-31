f50519dc1d47ca36f4976c7e397837c67d859038
Dave Biersach <dbiersach@gmail.com>
2020-05-31 07:29:09 +0000

    Fix Session 27

--- parallel-simpsons/main.cpp
+++ parallel-simpsons/main.cpp
@@ -1,13 +1,15 @@
 #include "stdafx.h"
 
 using namespace std;
+using namespace std::chrono;
 
-struct TCB {
-    std::thread* t = nullptr;
-    double a{};
-    double b{};
-    long intervals{};
-    double integral{};
+struct TCB
+{
+  std::thread *t = nullptr;
+  double a{};
+  double b{};
+  long intervals{};
+  double integral{};
 };
 
 // We want to integrate the unit circle
@@ -17,78 +19,85 @@ double g_b{1.0};
 
 inline double f(double x)
 {
-    return sqrt(1 - pow(x, 2));
+  return sqrt(1 - pow(x, 2));
 }
 
-void simpsons(TCB* tcb)
+void simpsons(TCB *tcb)
 {
-    double sum = f(tcb->b) + f(tcb->a);
-    double deltaX = (tcb->b - tcb->a) / tcb->intervals;
+  double sum = f(tcb->b) + f(tcb->a);
+  double deltaX = (tcb->b - tcb->a) / tcb->intervals;
+  tcb->a += deltaX;
+  for (int i = 1; i < tcb->intervals; i++)
+  {
+    int coeff = 2 * (i % 2 + 1);
+    double y = f(tcb->a);
+    if (!isnan(y))
+      sum += coeff * y;
     tcb->a += deltaX;
-    for (int i=1; i < tcb->intervals; i++) {
-        int coeff = 2 * (i % 2 + 1);
-        double y=f(tcb->a);
-        if (!isnan(y)) sum += coeff*y;
-        tcb->a += deltaX;
-    }
-    tcb->integral = (deltaX / 3) * sum;
+  }
+  tcb->integral = (deltaX / 3) * sum;
 }
 
 int main()
 {
-    int max_threads = thread::hardware_concurrency();
+  int max_threads = thread::hardware_concurrency();
 
-    cout.imbue(std::locale("en_US.UTF-8"));
+  cout.imbue(locale(""));
 
-    cout << "This session can support " << max_threads
-         << " simultaneous threads" << endl << endl;
+  cout << "This session can support "
+       << max_threads << " simultaneous threads" << endl
+       << endl;
 
-    cout << setw(15) << "Integral"
-         << setw(10) << "Threads"
-         << setw(10) << "Time" << endl;
+  cout << setw(15) << "Integral"
+       << setw(10) << "Threads"
+       << setw(10) << "Time"
+       << endl;
 
-    for (int threads{1}; threads <= max_threads * 4; threads *= 2) {
+  for (int threads{1}; threads <= max_threads * 4; threads *= 2)
+  {
+    auto startTime = steady_clock::now();
 
-        auto startTime = chrono::steady_clock::now();
+    double interval_width = (g_b - g_a) / threads;
 
-        double interval_width = (g_b - g_a) / threads;
+    // Each thread gets its own
+    // thread control block (TCB)
+    vector<TCB *> threadPool;
 
-        // Each thread gets its own
-        // thread control block (TCB)
-        vector<TCB*> threadPool;
-
-        for (int i{}; i < threads; i++) {
-            TCB* tcb = new TCB();
-            tcb->a = i * interval_width;
-            tcb->b = tcb->a + interval_width;
-            tcb->intervals = 134217728 / threads;  //2^27
-            tcb->t = new std::thread(simpsons, tcb);
-            // New thread is now running, so add its TCB
-            threadPool.push_back(tcb);
-        }
-
-        double integral{};
-        for (auto& tcb : threadPool)
-        {
-            // Wait here for this thread to finish
-            tcb->t->join();
-            // Now we read this thread's result
-            integral += tcb->integral;
-            delete tcb->t;
-         }
-         threadPool.clear();
-
-        auto endTime = chrono::steady_clock::now();
-
-        auto totalTime = chrono::duration_cast<chrono::milliseconds>
-            (endTime - startTime).count();
-
-        cout << setw(15) << fixed << setprecision(11)
-             << integral * 4.0
-             << setw(10) << threads
-             << setw(10) << fixed << setprecision(3)
-             << totalTime << "(ms)" << endl;
+    for (int i{}; i < threads; i++)
+    {
+      TCB *tcb = new TCB();
+      tcb->a = i * interval_width;
+      tcb->b = tcb->a + interval_width;
+      tcb->intervals = 134217728 / threads; // 2^27
+      tcb->t = new thread(simpsons, tcb);
+      // New thread is now running, so add its TCB
+      threadPool.push_back(tcb);
     }
 
-    return 0;
+    double integral{};
+    for (auto &tcb : threadPool)
+    {
+      // Wait here for this thread to finish
+      tcb->t->join();
+      // Now we read this thread's result
+      integral += tcb->integral;
+      delete tcb->t;
+    }
+    threadPool.clear();
+
+    auto endTime = steady_clock::now();
+
+    auto totalTime =
+        duration_cast<milliseconds>(endTime - startTime)
+            .count();
+
+    cout << fixed << setprecision(11);
+    cout << setw(15) << integral * 4.0
+         << setw(10) << threads
+         << setw(10) << setprecision(3)
+         << totalTime << "(ms)"
+         << endl;
+  }
+
+  return 0;
 }
