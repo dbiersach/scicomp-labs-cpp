635b509f20e13ec53b283c927e1be2935f3efed5
David Biersach <dbiersach@gmail.com>
2020-08-25 22:02:43 -0400

    Simplified Session 13 and clang-format

--- nyquist_known/nyquist_known.cpp
+++ nyquist_known/nyquist_known.cpp
@@ -2,51 +2,47 @@
 
 using namespace std;
 
-const double b = 20;
 const int n = 640;
 
 vector<double> x(n + 1);
 vector<double> y(n + 1);
 
-void InitData()
-{
-    double dx = b / n;
+void InitData(double a, double b) {
+  double dx = (b - a) / n;
 
-    for (int i = 0; i <= n; i++)
-    {
-        x.at(i) = i * dx;
-        y.at(i) = sin(4.0 / 5.0 * M_PI * x.at(i));
-    }
+  for (int i = 0; i <= n; i++) {
+    x.at(i) = a + i * dx;
+    y.at(i) = sin(4.0 / 5.0 * M_PI * x.at(i));
+  }
 }
 
-void nyquist_known()
-{
-    InitData();
+void nyquist_known() {
+  InitData(0, 20);
 
-    TCanvas* c = new TCanvas(
-        (string().append("Sinusoid Aliasing (")
-         .append(to_string(n))
-         .append(" samples)")).c_str());
+  TCanvas* c = new TCanvas((string()
+                                .append("Sinusoid Aliasing (")
+                                .append(to_string(n))
+                                .append(" samples)"))
+                               .c_str());
 
-    TMultiGraph* mg = new TMultiGraph();
-    string title = "y=sin(#frac{4#pi}{5}x) ("
-                   + to_string(n) + " samples)";
-    mg->SetTitle(title.c_str());
-    gStyle->SetTitleFontSize(0.03f);
+  TMultiGraph* mg = new TMultiGraph();
+  string title = "y=sin(#frac{4#pi}{5}x) (" + to_string(n) + " samples)";
+  mg->SetTitle(title.c_str());
+  gStyle->SetTitleFontSize(0.03f);
 
-    mg->SetMinimum(-1.);
-    mg->SetMaximum(1.);
+  mg->SetMinimum(-1.);
+  mg->SetMaximum(1.);
 
-    TGraph* g = new TGraph(n + 1, x.data(), y.data());
-    g->SetLineColor(kBlue);
-    g->SetMarkerColor(kRed);
-    g->SetMarkerStyle(kFullDotLarge);
+  TGraph* g = new TGraph(n + 1, x.data(), y.data());
+  g->SetLineColor(kBlue);
+  g->SetMarkerColor(kRed);
+  g->SetMarkerStyle(kFullDotLarge);
 
-    mg->Add(g);
-    mg->Draw("ALP");
+  mg->Add(g);
+  mg->Draw("ALP");
 
-    mg->GetXaxis()->SetTitle("x");
-    mg->GetYaxis()->SetTitle("y");
+  mg->GetXaxis()->SetTitle("x");
+  mg->GetYaxis()->SetTitle("y");
 
-    gPad->Modified();
+  gPad->Modified();
 }
--- nyquist_unknown/nyquist_unknown.cpp
+++ nyquist_unknown/nyquist_unknown.cpp
@@ -2,56 +2,48 @@
 
 using namespace std;
 
-const double b = 210;
 const int n = 420;
 
 vector<double> x(n + 1);
 vector<double> y(n + 1);
 
-void InitData()
-{
-    double dx = b / n;
+void InitData(double a, double b) {
+  double dx = (a + b) / n;
 
-    for (int i = 0; i <= n; i++)
-    {
-        x.at(i) = i * dx;
-        y.at(i) =
-            sin(4 * M_PI * x.at(i)) *
-            sin(6 * M_PI * x.at(i)) *
-            sin(10 * M_PI * x.at(i))*
-            sin(14 * M_PI * x.at(i));
-
-    }
+  for (int i = 0; i <= n; i++) {
+    x.at(i) = a + i * dx;
+    y.at(i) = sin(4 * M_PI * x.at(i)) * sin(6 * M_PI * x.at(i)) *
+              sin(10 * M_PI * x.at(i)) * sin(14 * M_PI * x.at(i));
+  }
 }
 
-void nyquist_unknown()
-{
-    InitData();
+void nyquist_unknown() {
+  InitData(0, 210);
 
-    TCanvas* c = new TCanvas(
-        (string().append("Sinusoid Aliasing (")
-         .append(to_string(n))
-         .append(" samples)")).c_str());
+  TCanvas* c = new TCanvas((string()
+                                .append("Sinusoid Aliasing (")
+                                .append(to_string(n))
+                                .append(" samples)"))
+                               .c_str());
 
-    TMultiGraph* mg = new TMultiGraph();
-    string title = "y=UNKNOWN ("
-                   + to_string(n) + " samples)";
-    mg->SetTitle(title.c_str());
-    gStyle->SetTitleFontSize(0.03f);
+  TMultiGraph* mg = new TMultiGraph();
+  string title = "y=UNKNOWN (" + to_string(n) + " samples)";
+  mg->SetTitle(title.c_str());
+  gStyle->SetTitleFontSize(0.03f);
 
-    mg->SetMinimum(-1.);
-    mg->SetMaximum(1.);
+  mg->SetMinimum(-1.);
+  mg->SetMaximum(1.);
 
-    TGraph* g = new TGraph(n + 1, x.data(), y.data());
-    g->SetLineColor(kBlue);
-    g->SetMarkerColor(kRed);
-    g->SetMarkerStyle(kFullDotLarge);
+  TGraph* g = new TGraph(n + 1, &x.at(0), &y.at(0));
+  g->SetLineColor(kBlue);
+  g->SetMarkerColor(kRed);
+  g->SetMarkerStyle(kFullDotLarge);
 
-    mg->Add(g);
-    mg->Draw("ALP");
+  mg->Add(g);
+  mg->Draw("ALP");
 
-    mg->GetXaxis()->SetTitle("x");
-    mg->GetYaxis()->SetTitle("y");
+  mg->GetXaxis()->SetTitle("x");
+  mg->GetYaxis()->SetTitle("y");
 
-    gPad->Modified();
+  gPad->Modified();
 }
